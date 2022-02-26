cd1c659f05ad125883f2ac84ddca0b9cc39c4839
ShYaGo <98923676+ShYaGo@users.noreply.github.com>
2022-02-26 16:46:04 -0500

    Update maa.c

--- usr/bin/maa.c
+++ usr/bin/maa.c
@@ -2,14 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#define BLK "\e[0;30m"
-#define RED "\e[4;31m"
-#define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-#define WHT "\e[0;37m"
-#define GRN "\e[0;32m"
+#include <ccolors.h>
 int main() {
 	char command[200];
 	printf(GRN "Hi, this is my well, not so awesome command line app. Commands: \"rand\" which prints a random number between 2 numbers, \"pasgen\" which is an undesired operation. \"time\" which prints the time. \"fileadd\" which adds a file.\n");
